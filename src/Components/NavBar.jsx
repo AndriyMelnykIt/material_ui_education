0c9f7b4642fbccd7d3b4c441ab0270f4bd18d97f
AndriyIt <andriy523625@gmail.com>
2021-12-07 21:24:20 +0200

    added form

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,23 +1,61 @@
-import React from 'react';
-import { Container, Toolbar, IconButton, Typography, AppBar, Box, Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+	Container, Toolbar, IconButton, Typography, AppBar, Box, Button, Dialog,
+	DialogTitle, DialogContent, TextField, DialogActions
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import { useStyles } from '../Styles/Styles';
 
 const NavBar = () => {
 	const classes = useStyles ();
+	const [open, setOpen] = useState (false);
+	const handleClickOpen = () => {
+		setOpen(true)
+	}
+	const handleClose = () => {
+		setOpen(false)
+	}
 
 	return (
 		<AppBar position='static'>
 			<Container>
 				<Toolbar>
 					<IconButton edge='start'
-					    color='inherit' aria-label='menu' className={classes.menuButton}>
+					            color='inherit' aria-label='menu' className={classes.menuButton}>
 						<MenuIcon/>
 					</IconButton>
 					<Typography variant='h6' className={classes.title}>Phillip Miller</Typography>
 					<Box mr={3}>
-						<Button color='inherit' variant='outlined'>Log In</Button>
+						<Button color='inherit' variant='outlined' onClick={handleClickOpen}>Log In</Button>
+
+						<Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+							<DialogTitle id="form-dialog-title">Log in</DialogTitle>
+							<DialogContent>
+								<DialogContent>Log in to see videos</DialogContent>
+								<TextField
+									autoFocus
+									margin="dense"
+									id="name"
+									label="Email Adress"
+									type="email"
+									fullWidth
+								/>
+								<TextField
+									autoFocus
+									margin="dense"
+									id="pass"
+									label="Password "
+									type="email"
+									fullWidth
+								/>
+							</DialogContent>
+							<DialogActions>
+								<Button onClick={handleClose} color="primary">Cancel</Button>
+								<Button onClick={handleClose} color="primary">Log in</Button>
+							</DialogActions>
+						</Dialog>
+
 					</Box>
 					<Button color='secondary' variant='contained'>Sign Up</Button>
 				</Toolbar>
